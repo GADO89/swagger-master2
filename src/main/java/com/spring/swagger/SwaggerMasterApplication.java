c3e4d38c6f11907b0d45ff055536f5464036521c
GADO89 <gadomahmoud377@gmail.com>
2023-11-26 01:53:26 +0200

    Create Project swagger-master2

---
+++ src/main/java/com/spring/swagger/SwaggerMasterApplication.java
@@ -0,0 +1,13 @@
+package com.spring.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerMasterApplication.class, args);
+	}
+
+}
